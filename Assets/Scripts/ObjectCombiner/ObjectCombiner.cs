774f5c9a7b4b77c8e18461f08cbfa75867a3aca5
Jan Julin <mummo94>
2018-05-31 16:32:15 +0300

    Added random torque when spawning object

--- Assets/Scripts/ObjectCombiner/ObjectCombiner.cs
+++ Assets/Scripts/ObjectCombiner/ObjectCombiner.cs
@@ -66,6 +66,8 @@ public class ObjectCombiner : MonoBehaviour
             if (item.GetComponent<Rigidbody>())
             {
                 item.GetComponent<Rigidbody>().AddForce(new Vector3(0, 3, -.5f), ForceMode.Impulse);
+
+                item.GetComponent<Rigidbody>().AddTorque(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
                 //item.GetComponent<Rigidbody>().isKinematic = true;x   
 
             }
