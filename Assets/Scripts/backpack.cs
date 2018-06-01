5abd275681f747f5cdf8cbab65f5c9e7033c673c
TYLL-GAME-G16\game <fabian.suomela@gmail.com>
2018-06-01 16:37:36 +0300

    fabi backpack test

--- Assets/Scripts/backpack.cs
+++ Assets/Scripts/backpack.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-public class backpack : MonoBehaviour {
-    public GameObject spawnObject;
+public class backpack : MonoBehaviour
+{
+public GameObject spawnObject;
 
-	
-    private void onTriggerStay(Collider collider)
+private void OnTriggerStay(Collider collider)
     {
-        VRTK_InteractGrab grabbingObject = (collider.gameobject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
-        if(CanGrab(grabbingObject))
+        VRTK_InteractGrab grabbingObject = (collider.gameObject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
+        if (CanGrab(grabbingObject))
         {
-            GameObject spawned = Instatiate(spawnObject);
-            grabbingObject.GetComponent<VRTK_InteractGrab>().ForceTouch(spawned);
-            grabbingObject.AttemptGrap();
-        }
-    }
-    private bool CanGrab(VRTK_InteractGrab grabbingObject)
+            Debug.Log("Spawning object");
+            
+            GameObject spawned = Instantiate(spawnObject);
+            grabbingObject.GetComponent<VRTK_InteractTouch>().ForceTouch(spawned);
+            grabbingObject.AttemptGrab();
+                   }
+           }
+
+	private bool CanGrab(VRTK_InteractGrab grabbingObject)
     {
-        return (grabbingObject && grabbingObject.GetGrabbedObject() == null && grabbingObject.gameobject.GetComponent<VRTK_ControllerEvents>().grabPressed);
-    }
+             return (grabbingObject && grabbingObject.GetGrabbedObject() == null && grabbingObject.gameObject.GetComponent<VRTK_ControllerEvents>().grabPressed);
+          }
 }
