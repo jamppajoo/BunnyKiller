b2d1c0670e61079355f7fbd7351dfcc0d05ae939
VR machine <Arktik.kirkle@gmail.com>
2018-06-01 15:43:49 +0300

    Radial menu testing

---
+++ Assets/Scripts/ObjectCombiner/RadialMenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenuController : MonoBehaviour {
+
+    public void SpawnItem1(int index)
+    {
+        print("Spawning item " + index); 
+    }
+
+}
