7a724ca3eee836953c79c093db69f7ec7538c3cd
TYLL-GAME-G16\game <fabian.suomela@gmail.com>
2018-06-04 14:39:13 +0300

    Fabi backpack test v2

---
+++ Assets/RWVR_InteractionController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RWVR_InteractionController : MonoBehaviour {
+
+    public Transform snapColliderOrigin; // 1
+    public GameObject ControllerModel; // 2
+
+    [HideInInspector]
+    public Vector3 velocity; // 3
+    [HideInInspector]
+    public Vector3 angularVelocity; // 4
+
+    private RWVR_InteractionObject objectBeingInteractedWith; // 5
+
+    private SteamVR_TrackedObject trackedObj; // 6
+
+    private SteamVR_Controller.Device Controller // 1
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    public RWVR_InteractionObject InteractionObject // 2
+    {
+        get { return objectBeingInteractedWith; }
+    }
+
+    void Awake() // 3
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void CheckForInteractionObject()
+    {
+        Collider[] overlappedColliders = Physics.OverlapSphere(snapColliderOrigin.position, snapColliderOrigin.lossyScale.x / 2f); // 1
+
+        foreach (Collider overlappedCollider in overlappedColliders) // 2
+        {
+            if (overlappedCollider.CompareTag("InteractionObject") && overlappedCollider.GetComponent<RWVR_InteractionObject>().IsFree()) // 3
+            {
+                objectBeingInteractedWith = overlappedCollider.GetComponent<RWVR_InteractionObject>(); // 4
+                objectBeingInteractedWith.OnTriggerWasPressed(this); // 5
+                return; // 6
+            }
+        }
+    }
+    void Update()
+    {
+        if (Controller.GetHairTriggerDown()) // 1
+        {
+            CheckForInteractionObject();
+        }
+
+        if (Controller.GetHairTrigger()) // 2
+        {
+            if (objectBeingInteractedWith)
+            {
+                objectBeingInteractedWith.OnTriggerIsBeingPressed(this);
+            }
+        }
+
+        if (Controller.GetHairTriggerUp()) // 3
+        {
+            if (objectBeingInteractedWith)
+            {
+                objectBeingInteractedWith.OnTriggerWasReleased(this);
+                objectBeingInteractedWith = null;
+            }
+        }
+
+    }
+    private void UpdateVelocity()
+    {
+        velocity = Controller.velocity;
+        angularVelocity = Controller.angularVelocity;
+    }
+
+    void FixedUpdate()
+    {
+        UpdateVelocity();
+    }
+    public void HideControllerModel()
+    {
+        ControllerModel.SetActive(false);
+    }
+
+    public void ShowControllerModel()
+    {
+        ControllerModel.SetActive(true);
+    }
+    public void Vibrate(ushort strength) // 1
+    {
+        Controller.TriggerHapticPulse(strength);
+    }
+
+    public void SwitchInteractionObjectTo(RWVR_InteractionObject interactionObject) // 2
+    {
+        objectBeingInteractedWith = interactionObject; // 3
+        objectBeingInteractedWith.OnTriggerWasPressed(this); // 4
+    }
+}
---
+++ Assets/RWVR_InteractionObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RWVR_InteractionObject : MonoBehaviour {
+
+    protected Transform cachedTransform; // 1
+    [HideInInspector] // 2
+    public RWVR_InteractionController currentController; // 3
+
+    public virtual void OnTriggerWasPressed(RWVR_InteractionController controller)
+    {
+        currentController = controller;
+    }
+
+    public virtual void OnTriggerIsBeingPressed(RWVR_InteractionController controller)
+    {
+    }
+
+    public virtual void OnTriggerWasReleased(RWVR_InteractionController controller)
+    {
+        currentController = null;
+    }
+    public virtual void Awake()
+    {
+        cachedTransform = transform; // 1
+        if (!gameObject.CompareTag("InteractionObject")) // 2
+        {
+            Debug.LogWarning("This InteractionObject does not have the correct tag, setting it now.", gameObject); // 3
+            gameObject.tag = "InteractionObject"; // 4
+        }
+    }
+    public bool IsFree() // 1
+    {
+        return currentController == null;
+    }
+
+    public virtual void OnDestroy() // 2
+    {
+        if (currentController)
+        {
+            OnTriggerWasReleased(currentController);
+        }
+    }
+}
