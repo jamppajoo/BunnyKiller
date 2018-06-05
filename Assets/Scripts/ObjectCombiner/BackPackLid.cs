46da06a3d4cb8ca13add627f95f67f14a83ba2ac
Jan Julin <mummo94>
2018-06-05 14:51:36 +0300

    Object backpack testing

---
+++ Assets/Scripts/ObjectCombiner/BackPackLid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPackLid : MonoBehaviour {
+
+    public float timeToMove = 0.5f;
+
+    private Vector3 closedPosition;
+    private Vector3 openPosition;
+
+    void Start()
+    {
+        closedPosition = gameObject.transform.localPosition;
+        openPosition = new Vector3(gameObject.transform.localPosition.x - (gameObject.transform.localScale.x - 0.1f), gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+        openLid();
+    }
+    public void openLid()
+    {
+        StartCoroutine(moveLid(openPosition, timeToMove));
+    }
+    public void closeLid()
+    {
+        StartCoroutine(moveLid(closedPosition, timeToMove));
+    }
+
+    IEnumerator moveLid(Vector3 newPosition, float time)
+    {
+        float elapsedTime = 0;
+        Vector3 currentPosition = gameObject.transform.localPosition;
+
+        while (elapsedTime < time)
+        {
+            gameObject.transform.localPosition = Vector3.Lerp(currentPosition, newPosition, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+    }
+}
