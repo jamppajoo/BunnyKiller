7fadac0866c1b3c09ef0d690b1da9aeab5e861f0
Jan Julin <mummo94@hotmail.com>
2018-06-07 18:57:47 +0300

    Made granade explosion with fragments

---
+++ Assets/Scripts/ObjectStuff/GranadeFragment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranadeFragment : MonoBehaviour {
+
+    public float fragmentDestroyTime = 3;
+
+	void Start () {
+        StartCoroutine(DestroyFragment());
+	}
+	
+	IEnumerator DestroyFragment()
+    {
+        yield return new WaitForSeconds(fragmentDestroyTime);
+        Destroy(gameObject);
+    }
+}
