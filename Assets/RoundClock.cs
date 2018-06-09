853f5ae42ec63c9c1a25645c8d15c01ca78a2ad6
FabianKoti <fabian.suomela@gmail.com>
2018-06-09 14:50:45 +0300

    Fabi test, round clock

---
+++ Assets/RoundClock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundClock : MonoBehaviour {
+
+    private bool roundStart = false;
+    public float timeLeft;
+
+    void OnTriggerStay(Collider Player)
+    {
+        Debug.Log("Round start");
+        roundStart = true;
+    }
+
+        void Update () {
+        if (roundStart == true)
+        {
+            timeLeft -= Time.time;
+        }
+        if (timeLeft == 0f)
+        {
+            Debug.Log("round is over");
+            return;
+        }
+    }
+}
