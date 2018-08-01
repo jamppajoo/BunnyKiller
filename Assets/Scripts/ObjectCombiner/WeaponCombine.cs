f9dcb9b5bf7cb46072ba18adb7f3051df4cfa152
Jan Julin <mummo94>
2018-08-01 13:54:40 +0300

    Weapon combine on baseballbat and barbed wire working

---
+++ Assets/Scripts/ObjectCombiner/WeaponCombine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class WeaponCombine : MonoBehaviour {
+
+
+
+    private VRTK_SnapDropZone dropZone;
+    private GameObject attachedObject;
+    private VRTK_PolicyList policyList;
+
+    void Start () {
+
+        dropZone = GetComponent<VRTK_SnapDropZone>();
+        policyList = GetComponent<VRTK_PolicyList>();
+
+        dropZone.ObjectSnappedToDropZone += new SnapDropZoneEventHandler(ObjectAttached);
+		
+	}
+
+    private void ObjectAttached(object sender, SnapDropZoneEventArgs e)
+    {
+        attachedObject = e.snappedObject;
+        DisableChildCollider();
+        DisableSnapDropZone();
+    }
+    private void DisableChildCollider()
+    {
+        attachedObject.gameObject.GetComponent<Collider>().enabled = false;
+    }
+    private void DisableSnapDropZone()
+    {
+        policyList.identifiers.Clear();
+    }
+}
