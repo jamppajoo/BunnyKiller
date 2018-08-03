86bb7a13d76ddfe1e8fdcdb47eeae6f6d9e87214
Kiviniemi <mrcap1@gmail.com>
2018-08-03 14:57:45 +0300

    testing trigger bat

--- Assets/Scripts/Bunny/HitBunny.cs
+++ Assets/Scripts/Bunny/HitBunny.cs
@@ -86,7 +86,8 @@ public class HitBunny : MonoBehaviour {
         if(other.gameObject.tag.ToString().Equals("Bunny"))
         {
             hittedObject = other.gameObject;
-            hittedObject.GetComponent<Movement>().HitBunny((lastPosition - transform.position), 10000f);
+            hittedObject.GetComponentInParent<Movement>().HitBunny((lastPosition - transform.position), 10000f);
+            
                 //.BaseballHit(hitPower);
             //            other.GetComponent<Movement>().HitBunny((lastPosition-transform.position), Vector3.Distance(lastPosition, transform.position));
             //other.gameObject.GetComponent<Movement>().HitBunny((lastPosition - transform.position), 10000f);
